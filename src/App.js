e5136d9fed624d3ffd2f51a671b4709430765b5e
Rahul Nayanegali <rahulnayanegali@gmail.com>
2020-04-06 22:21:44 +0530

    Functionality Done

--- src/App.js
+++ src/App.js
@@ -1,54 +1,171 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef, useLayoutEffect} from 'react';
 import './App.css';
 import Aux from './Aux/Aux';
 import GameInfoContainer from './containers/gameInfoContainer/GameInfoContainer'
 import Card from './containers/cards/Card/Card';
 
 function App() {
+
+    const [noMatch, setNoMatch] = useState();
   const [startGame, setstartGame] = useState(false);
-  const [display, setDisplay] = useState();
-  const [frontFaces, setFrontFaces] = useState([])
+  const [frontFaces, setFrontFaces] = useState();
+  const [count, setCount] = useState(0);
+  const [Bat, setBat] = useState(false)
+  const [bat, setbat] = useState()
+  const [Bones, setBones] = useState(false)
+  const [bones, setbones] = useState()
+  const [Cauldron, setCauldron] = useState(false)
+  const [cauldron, setNoCauldron] = useState(false)
+  const [Dracula, setDracula] = useState(false)
+  const [dracula, setNoDracula] = useState(false)
+  const [Eye, setEye] = useState(false)
+  const [eye, setNoEye] = useState(false)
+  const [Ghost, setGhost] = useState(false)
+  const [ghost, setNoGhost] = useState(false)
+  const [Pumpkin, setPumpkin] = useState(false)
+  const [pumpkin, setNoPumpkin] = useState(false)
+  const [Skull, setSkull] = useState(false)
+  const [skull, setNoSkull] = useState(false)
+  const [faces, setFaces] = useState(shuffle(['Bat', 'Bat', 'Bones', 'Bones', 'Cauldron', 'Cauldron',
+              'Dracula', 'Dracula', 'Eye', 'Eye', 'Ghost', 'Ghost', 'Pumpkin', 'Pumpkin', 'Skull', 'Skull' ]))
+  
 
-  const cardClickHandler = (frontFace) => {
-    // const frontFace = frontFace;
-    setFrontFaces([ ...frontFaces, {
-      id: frontFaces.length,
-      value: frontFace
-    }])
 
+  function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    });
+    return ref.current;
+  }
+
+  function shuffle(array) {
+    var m = array.length, t, i;
+  
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
     }
+  
+    return array;
+  }
+
+//   console.log(faces);
+//   console.log(shuffle(faces))
+  const misMatch = (frontFaces, prevFace) => {
+
+  }
+  const cardClickHandler = (frontFace) => {
+    setCount(count + 1);
+    setFrontFaces(frontFace);
+    }
+    const prevFace = usePrevious(frontFaces)
+
+    useEffect(() => {
+        
+        if( count === 2) {
+            if (frontFaces === prevFace) {
+            switch (frontFaces) {
+                case ('Bat'):
+                    setBat(true);
+                    break;
+
+                case ('Bones'):
+                    setBones(true);
+                    break;
+                
+                case ('Cauldron'):
+                    setCauldron(true);
+                    break;
+
+                case ('Dracula'):
+                    setDracula(true);
+                    break;
+
+                case ('Eye'):
+                    setEye(true);
+                    break;
+
+                case ('Ghost'):
+                    setGhost(true);
+                    break;
+
+                case ('Pumpkin'):
+                    setPumpkin(true);
+                    break;
+
+                case ('Skull'):
+                    setSkull(true);
+                    break;
+
+                default:
+                    console.log('default');
+            }
+            } else {
+                setTimeout(() => {
+                    console.log('This will run after 1 second!')
+                    // setNoMatch(true)
+                    // noMatch = true
+                    setCount(0)
+                    // setbat(true)
+                    // setbones(true)
+                    setNoMatch(true)
+                    // (set`${eval(frontFaces.toLowerCase())}`(true))
+                    // setbat(false)
+                    // setbones(false)
+                    setNoMatch(false)
+                    }, 500);
+            }
+        }
+        setCount(0)
+    }, [prevFace]);
+
+    useLayoutEffect(() => {
+        
+    }, [])
 
   return (
     <Aux>
-    <p>{console.log(frontFaces)}</p>
       {   
         startGame ? null : <div className="overlayText" onClick={() => setstartGame(true)}>Click to Start</div>
       } 
-      <h1 onClick={() => console.log("clicked")} className="pageTitle" >Mix-Or-Match</h1>
+      <h1  className="pageTitle" >Mix-Or-Match</h1>
 
       <GameInfoContainer />
 
       <div className="cardsContainer" >
-                <Card display={display} frontImage={'Bat'} clicked={cardClickHandler} /> 
-                <Card display={display} frontImage={'Bat'} clicked={cardClickHandler} />
-                <Card display={display} frontImage={'Bones'} clicked={cardClickHandler}/>
-                <Card display={display} frontImage={'Bones'}/> 
-                <Card display={display} frontImage={'Cauldron'}/>
-                <Card display={display} frontImage={'Cauldron'}/>
-                <Card display={display} frontImage={'Dracula'}/>
-                <Card display={display} frontImage={'Dracula'}/>
-                <Card display={display} frontImage={'Eye'}/>
-                <Card display={display} frontImage={'Eye'}/>
-                <Card display={display} frontImage={'Ghost'}/>
-                <Card display={display} frontImage={'Ghost'}/>
-                <Card display={display} frontImage={'Pumpkin'}/>
-                <Card display={display} frontImage={'Pumpkin'}/>
-                <Card display={display} frontImage={'Skull'}/> 
-                <Card display={display} frontImage={'Skull'}/>
-            </div>
 
+      {faces.map(function(name, index){
+      return <Card key={index} frontImage={`${name}`} clicked={cardClickHandler} clickTimes={count} matched={eval(name)} noMatch={noMatch} /> 
+                    })}
+
+          {/* <Card frontImage={'Bat'} clicked={cardClickHandler} clickTimes={count} matched={bat} noMatch={bat}/> 
+          <Card frontImage={'Bat'} clicked={cardClickHandler} clickTimes={count} matched={bat} noMatch={noBat}/>
+          <Card frontImage={'Bones'} clicked={cardClickHandler} clickTimes={count} matched={bones} noMatch={bones}/>
+          <Card frontImage={'Bones'} clicked={cardClickHandler} clickTimes={count} matched={bones} noMatch={noBones}/> 
+          <Card frontImage={'Cauldron'} clicked={cardClickHandler} clickTimes={count} matched={cauldron} noMatch={noCauldron}/>
+          <Card frontImage={'Cauldron'} clicked={cardClickHandler} clickTimes={count} matched={cauldron} noMatch={noCauldron}/>
+          <Card frontImage={'Dracula'} clicked={cardClickHandler} clickTimes={count} matched={dracula} noMatch={noDracula}/>
+          <Card frontImage={'Dracula'} clicked={cardClickHandler} clickTimes={count} matched={dracula} noMatch={noDracula}/>
+          <Card frontImage={'Eye'} clicked={cardClickHandler} clickTimes={count} matched={eye} noMatch={noEye}/>
+          <Card frontImage={'Eye'} clicked={cardClickHandler} clickTimes={count} matched={eye} noMatch={noEye}/>
+          <Card frontImage={'Ghost'} clicked={cardClickHandler} clickTimes={count} matched={ghost} noMatch={noGhost}/>
+          <Card frontImage={'Ghost'} clicked={cardClickHandler} clickTimes={count} matched={ghost} noMatch={noGhost}/>
+          <Card frontImage={'Pumpkin'} clicked={cardClickHandler} clickTimes={count} matched={pumpkin} noMatch={noPumpkin}/>
+          <Card frontImage={'Pumpkin'} clicked={cardClickHandler} clickTimes={count} matched={pumpkin} noMatch={noPumpkin}/>
+          <Card frontImage={'Skull'} clicked={cardClickHandler} clickTimes={count} matched={skull} noMatch={noSkull}/> 
+          <Card frontImage={'Skull'} clicked={cardClickHandler} clickTimes={count} matched={skull} noMatch={noSkull}/> */}
+      </div>
     </Aux>
   );
 }
-
+// frontFaces.length === 2 && (name === frontFaces[1].value)
+// eval(name.toLowerCase())
 export default App;
--- src/containers/cards/Card/Card.js
+++ src/containers/cards/Card/Card.js
@@ -1,46 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Card.css';
 import Aux from '../../../Aux/Aux';
 
 function Card(props) {
 
-    const [display, setDisplay] = useState(false) 
     const frontFaceImageUrl = props.frontImage;
     const [count, setCount] = useState(0)
-    // const display = false;
+    const clickTimes = props.clickTimes;
+    // const [matched, setMatched] = useState(props.matched);
+    const [rotate, setRotate] = useState(false);
 
-    const clickFlipHandler = () => {
-        setCount(count + 1)
-        props.clicked(frontFaceImageUrl)
-        setDisplay(true)
+    const clickFlipHandler = (event) => {
+        // console.log(event.currentTarget)
+        
+        console.log(typeof(event.currentTarget.className))
+        console.log(event.currentTarget)
+        if (props.matched) {
+            
+        } else {
+        if (clickTimes < 2 && count < 1 ) {
+            setCount(count + 1)
+            console.log(count)
+            // count = count +1;
+            // console.log('clicked')
+            props.clicked(frontFaceImageUrl)
+            // event.currentTarget.classList = [ "card rotate"]
+            event.currentTarget.className = "card rotate"
+            // setRotate(true) 
+            // console.log(props.clickTimes)
+            }
+        }
     }
-
+    useEffect(() => {
+        if(count === 1) {
+            setCount(0)
+        }
+    })
     return ( 
         <Aux>
-        <div className= "card" onClick={clickFlipHandler}>
+        <div className= {`card ${props.matched? `matched rotate`: ``} ${props.noMatch? ``: null}`}  onClick={clickFlipHandler}>
             
-            {
-                display ?
-            
-                <div className="cardFront cardFace ">
-                    <img className="cobWeb cobWebTopRight" src={ require('../../../Assets/Images/CobwebGrey.png')} />
-                    <img className="cobWeb cobWebTopLeft" src={ require('../../../Assets/Images/CobwebGrey.png')} />
-                    <img className="cobWeb cobWebBottomRight" src={ require('../../../Assets/Images/CobwebGrey.png')} />
-                    <img className="cobWeb cobWebBottomLeft" src={ require('../../../Assets/Images/CobwebGrey.png')} />
-                    <img className="cardValue" src={ require(`../../../Assets/Images/${frontFaceImageUrl}.png`)}/>
-                </div>:
-                <div className= " cardBack cardFace">
-                    <img className="cobWeb cobWebTopRight" src={ require('../../../Assets/Images/Cobweb.png')} />
-                    <img className="cobWeb cobWebTopLeft" src={ require('../../../Assets/Images/Cobweb.png')} />
-                    <img className="cobWeb cobWebBottomRight" src={ require('../../../Assets/Images/Cobweb.png')} />
-                    <img className="cobWeb cobWebBottomLeft" src={ require('../../../Assets/Images/Cobweb.png')} />
-                    <img className="spider" src={ require('../../../Assets/Images/Spider.png')}/>
+                 <div className={`cardFront cardFace `}>
+                    <img className="cobWeb cobWebTopRight" alt='bottom-right' src={ require('../../../Assets/Images/CobwebGrey.png')} />
+                    <img className="cobWeb cobWebTopLeft" alt='bottom-left' src={ require('../../../Assets/Images/CobwebGrey.png')} />
+                    <img className="cobWeb cobWebBottomRight" alt='bottom-right' src={ require('../../../Assets/Images/CobwebGrey.png')} />
+                    <img className="cobWeb cobWebBottomLeft" alt='bottom-left' src={ require('../../../Assets/Images/CobwebGrey.png')} />
+                    <img className="cardValue" alt='face' src={ require(`../../../Assets/Images/${frontFaceImageUrl}.png`)}/>
                 </div>
-                }
-            
+                
+                <div className= {`cardBack cardFace `}>
+                    <img className="cobWeb cobWebTopRight" alt='bottom-right' src={ require('../../../Assets/Images/Cobweb.png')} />
+                    <img className="cobWeb cobWebTopLeft" alt='bottom-left' src={ require('../../../Assets/Images/Cobweb.png')} />
+                    <img className="cobWeb cobWebBottomRight" alt='bottom-right' src={ require('../../../Assets/Images/Cobweb.png')} />
+                    <img className="cobWeb cobWebBottomLeft" alt='bottom-left' src={ require('../../../Assets/Images/Cobweb.png')} />
+                    <img className="spider" alt='back' src={ require('../../../Assets/Images/Spider.png')}/>
+                </div>           
         </div>
         </Aux>
     )
 }
-
 export default Card;
